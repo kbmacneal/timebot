24fa81863bb7803feffffec959f74c149187e46e
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-08-28 23:20:17 -0500

    added data structure for swnbot integration

---
+++ Classes/cls_swnbot.cs
@@ -0,0 +1,55 @@
+namespace timebot.Classes
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class SwnbotResponse
+    {
+        [JsonProperty("userID")]
+        public string UserId { get; set; }
+
+        [JsonProperty("userName")]
+        public string UserName { get; set; }
+
+        [JsonProperty("userNick")]
+        public object UserNick { get; set; }
+
+        [JsonProperty("userRoles")]
+        public UserRole[] UserRoles { get; set; }
+    }
+
+    public partial class UserRole
+    {
+        [JsonProperty("roleID")]
+        public string RoleId { get; set; }
+
+        [JsonProperty("roleName")]
+        public string RoleName { get; set; }
+    }
+
+    public partial class SwnbotResponse
+    {
+        public static SwnbotResponse FromJson(string json) => JsonConvert.DeserializeObject<SwnbotResponse>(json, timebot.Classes.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this SwnbotResponse self) => JsonConvert.SerializeObject(self, timebot.Classes.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters = {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
