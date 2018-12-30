7a66adbc2c4af33ec82aad64a0d583513656a6e1
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-12-29 22:31:32 -0600

    adding debug to the timer output

--- Classes/cls_new_years.cs
+++ Classes/cls_new_years.cs
@@ -2,108 +2,106 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Dynamic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using JsonFlatFileDataStore;
 using Newtonsoft.Json;
-using System.Threading;
-using System.Globalization;
 using NodaTime;
 
-namespace timebot.Classes
-{
-    public class new_years
-    {
-        public static List<Times> event_list = new List<Times>();
+namespace timebot.Classes {
+    public class new_years {
 
-        public static async Task<List<string>> schedule_times(SocketCommandContext context)
-        {
+        public static List<Timer> timers {get;set;} = new List<Timer>();
+        public static async Task<List<string>> schedule_times (SocketCommandContext context) {
 
             // Times.FromJson("new_years.json").ToList();
 
-            List<string> rtn = new List<string>();
+            List<string> rtn = new List<string> ();
 
-            var event_list = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Times>>(System.IO.File.ReadAllText("new_years.json"));
+            var event_list = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Times>> (System.IO.File.ReadAllText ("new_years.json"));
 
             // await ScheduleAction(context, null);
 
-            event_list.ForEach(async e => {
-                await ScheduleAction(context, e);
-                rtn.Add(e.Countries);
-            }); 
+            List<string> times = new List<string> ();
 
+            event_list.ForEach (async e => {
+                await ScheduleAction (context, e);
+                times.Add ("Scheduled " + e.ToString());
+                rtn.Add (e.Countries);
+            });
+
+            await System.IO.File.WriteAllLinesAsync ("timers_scheduled.txt", times);
+            System.IO.File.AppendAllText("timers_scheduled.txt","\nTimers Scheduled:");
+            System.IO.File.AppendAllText("timers_scheduled.txt","\n" + timers.Count().ToString());
 
             return rtn;
 
         }
 
-        public static Timer CreateTimer(SocketCommandContext con, Times t)
-        {
+        public static Timer CreateTimer (SocketCommandContext con, Times t) {
+            var tmr = new Timer (async x => await new_year_callback (con, t));
 
-            return new Timer(async x => await new_year_callback(con, t));
+            timers.Add(tmr);
+
+            return tmr;
         }
 
+        public static async Task ScheduleAction (SocketCommandContext con, Times e) {
+            var t = CreateTimer (con, e);
 
-        public static async Task ScheduleAction(SocketCommandContext con, Times e)
-        {
-            var t = CreateTimer(con, e);
+            Instant now = SystemClock.Instance.GetCurrentInstant ();
 
-            Instant now = SystemClock.Instance.GetCurrentInstant();
+            ZonedDateTime now_zoned = new ZonedDateTime (now, DateTimeZoneProviders.Tzdb.GetSystemDefault ());
 
-            ZonedDateTime now_zoned = new ZonedDateTime(now, DateTimeZoneProviders.Tzdb.GetSystemDefault());
-
-            NodaTime.ZonedDateTime dt = new NodaTime.ZonedDateTime(Instant.FromDateTimeOffset(new DateTimeOffset(DateTime.Parse(e.CentralStandardTime))), NodaTime.DateTimeZone.ForOffset(NodaTime.Offset.FromHours(-6)), NodaTime.CalendarSystem.Gregorian);
+            NodaTime.ZonedDateTime dt = new NodaTime.ZonedDateTime (Instant.FromDateTimeOffset (new DateTimeOffset (DateTime.Parse (e.CentralStandardTime))), NodaTime.DateTimeZone.ForOffset (NodaTime.Offset.FromHours (-6)), NodaTime.CalendarSystem.Gregorian);
 
             Duration interval = dt - now_zoned;
 
-            t.Change((int)interval.TotalMilliseconds, Timeout.Infinite);
+            t.Change ((int) interval.TotalMilliseconds, Timeout.Infinite);
 
-            // t.Change(10000, Timeout.Infinite);
         }
 
-        public static async Task new_year_callback(SocketCommandContext con, Times t)
-        {
-            //  var channels = await con.Guild.GetChannelsAsync();
-            //  ISocketMessageChannel out_channel = (ISocketMessageChannel)con.Guild.GetChannelAsync(channels.FirstOrDefault(e=>e.Name == "new-years-announcements").Id).GetAwaiter().GetResult();
+        public static async Task new_year_callback (SocketCommandContext con, Times t) {
 
-            var out_channel = con.Guild.GetChannel(con.Guild.Channels.First(e => e.Name == "new-years-announcements").Id) as ISocketMessageChannel;
+            var out_channel = con.Guild.GetChannel (con.Guild.Channels.First (e => e.Name == "new-years-announcements").Id) as ISocketMessageChannel;
 
             if (out_channel == null) return;
 
-            List<string> output = new List<string>();
-            output.Add(t.CentralStandardTime);
-            output.Add(t.Countries);
-            output.Add(t.Cities);
+            List<string> output = new List<string> ();
+            output.Add ("HAPPY NEW YEAR!");
+            output.Add (t.CentralStandardTime);
+            output.Add (t.Countries);
+            output.Add (t.Cities);
 
-            await out_channel.SendMessageAsync(String.Join(System.Environment.NewLine,output), false, null, new RequestOptions { RetryMode = RetryMode.RetryRatelimit });
+            await out_channel.SendMessageAsync (String.Join (System.Environment.NewLine, output), false, null, new RequestOptions { RetryMode = RetryMode.RetryRatelimit });
         }
     }
 
-
-
-    public partial class Times
-    {
-        [JsonProperty("Central_Standard_Time")]
+    public partial class Times {
+        [JsonProperty ("Central_Standard_Time")]
         public string CentralStandardTime { get; set; }
 
-        [JsonProperty("Countries")]
+        [JsonProperty ("Countries")]
         public string Countries { get; set; }
 
-        [JsonProperty("Cities")]
+        [JsonProperty ("Cities")]
         public string Cities { get; set; }
+
+        public override string ToString()        
+        {
+            return this.CentralStandardTime + " " + this.Countries + " " + this.Cities;
+        }
     }
 
-    public partial class Times
-    {
-        public static Times[] FromJson(string json) => JsonConvert.DeserializeObject<Times[]>(json, Converter.Settings);
+    public partial class Times {
+        public static Times[] FromJson (string json) => JsonConvert.DeserializeObject<Times[]> (json, Converter.Settings);
     }
 }
-
-
-
