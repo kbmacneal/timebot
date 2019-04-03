ef65690f148113d4be22f12cdad1100474fb0b02
Ken MacNeal <kenneth.b.macneal@gmail.com>
2019-04-03 16:08:44 -0500

    trying to replace reflection with efcore

---
+++ Context/EFCoreContext.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using timebot.Classes.Assets;
+
+namespace timebot.Context
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Asset> Assets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        { 
+            Dictionary<string, string> secrets = JsonConvert.DeserializeObject<Dictionary<string, string>>(System.IO.File.ReadAllText(Program.secrets_file));
+            
+            optionsBuilder.UseNpgsql(secrets["connection_string"]);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2l.2.0-rtm-35687");
+
+            modelBuilder.Entity<Asset>(entity =>
+            {
+                entity.Property(e=>e.Attack).HasColumnName("Attack");
+
+            });
+        }
+    }
+}
