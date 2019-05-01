0b6e3ab1f904910143e94dcda6f84ff28bfd0481
Ken MacNeal <kenneth.b.macneal@gmail.com>
2019-05-01 14:55:28 -0500

    keeping migrations consistent

--- Migrations/20190501192824_adding_private_faction_list.Designer.cs
+++ Migrations/20190501192824_adding_private_faction_list.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using timebot.Contexts;
@@ -9,9 +10,10 @@ using timebot.Contexts;
 namespace timebot.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20190501192824_adding_private_faction_list")]
+    partial class adding_private_faction_list
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20190501192824_adding_private_faction_list.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace timebot.Migrations
+{
+    public partial class adding_private_faction_list : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Factions",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    factionname = table.Column<string>(nullable: true),
+                    factionshortname = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Factions", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Factions");
+        }
+    }
+}
